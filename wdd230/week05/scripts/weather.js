d38ee4bee2f6ba0d39c87737a3731621a0588cc2
Clementine365 <97380639+Clementine365@users.noreply.github.com>
2023-10-12 10:12:55 +0300

    Update weather.js

--- wdd230/week05/scripts/weather.js
+++ wdd230/week05/scripts/weather.js
@@ -2,24 +2,24 @@ const currentTemp= document.querySelector('#current-temp');
 const weatherIcon= document.querySelector('#weather-icon');
 const captionDesc= document.querySelector('#figcaption');
 
-// Declare a const variable named "url" with your OpenWeatherMap API key
-const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
-const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
-const city = 'Trier,DE'; // Replace with your desired location
-const units = 'imperial'; // You can change to 'metric' for Celsius
 
-const url = `${baseUrl}?q=${city}&units=${units}&appid=${apiKey}`;
+const apiKey = '1a98fdfe973bd0187ad8973c43a140f4'; 
+const baseUrl = 'https://openweathermap.org/find?utf8=%E2%9C%93&q=kigali%2C+rwanda';
+const city = 'kigali'; 
+const units = 'imperial';
+
+const url = `https://openweathermap.org/find?utf8=%E2%9C%93&q=kigali%2C+rwanda`;
 
 
 
-// Define an asynchronous function named "apiFetch()"
+
 async function apiFetch() {
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      console.log(data); // Testing only
-      displayResults(data); // Call the displayResults function
+      console.log(data); 
+      displayResults(data); 
     } else {
       throw Error(await response.text());
     }
@@ -28,9 +28,9 @@ async function apiFetch() {
   }
 }
 
-apiFetch(); // Invoke the apiFetch function
+apiFetch(); 
 
-// Build the displayResults function to display weather data
+
 function displayResults(data) {
   currentTemp.innerHTML = `${data.main.temp}&deg;F`;
   const icon = data.weather[0].icon;
